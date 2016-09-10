788da6b2890c48d103276caa3413a7b6502db22c
Tedmund Chua <tchua93@gmail.com>
2016-09-10 16:08:02 -0700

    fa - creates controllers and views for users and proposals

--- app/controllers/proposals_controller.rb
+++ app/controllers/proposals_controller.rb
@@ -1,1 +1,39 @@
-proposals_controller.rb
+get '/proposals' do
+  @proposals = Proposal.all 
+  erb :'proposals/index' 
+end
+
+
+get '/proposals/new' do
+  erb :'proposals/new'
+end
+
+post '/proposals' do
+  @proposal = Proposal.new(params[:proposal]) 
+  if @proposal.save 
+    redirect '/proposals' 
+  else
+    erb :'proposals/new' 
+  end
+end
+
+get '/proposals/:id/edit' do
+  @proposal = Proposal.find(params[:id]) 
+  erb :'proposals/edit'
+end
+
+put '/proposals/:id' do
+  @proposal = Proposal.find(params[:id]) 
+  @proposal.assign_attributes(params[:proposal]) 
+  if @proposal.save 
+    redirect '/proposals' 
+  else
+    erb :'proposals/edit' 
+  end
+end
+
+delete '/proposals/:id' do
+  @proposal = Proposal.find(params[:id]) 
+  @proposal.destroy 
+  redirect '/proposals' 
+end
