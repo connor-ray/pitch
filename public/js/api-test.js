2a629fde8141405e02673b0da290884724f7b8df
Connor Ray <connor.ray.cb@gmail.com>
2016-09-11 06:58:13 -0700

    Adding list to panel

--- public/js/api-test.js
+++ public/js/api-test.js
@@ -2,14 +2,73 @@ $(document).ready(function() {
   getNearbyLocations();
 });
 
+var generateMap = function(coordinates) {
+  var mapurl = "https://image.maps.cit.api.here.com/mia/1.6/mapview?";
+  var currentLocation = 'c=37.7719,-122.4218&';
+  var app_id = '&app_id=fRTOmoR7FfcvAIuCqt1V&';
+  var app_code = 'app_code=X1FkzDPPRfhlg2cjfTbT2w';
+  // coordinates.slice
+  console.log(coordinates)
+  var poi = 'poi=' + coordinates;
+  // console.log(poi)
+
+  var dataUrl = mapurl + currentLocation + poi + app_id + app_code;
+  console.log(dataUrl);
+
+
+  $.ajax({
+    url: 'map_url',
+    type: 'get',
+    data: {url: dataUrl},
+
+    success: function (data) {
+      json = JSON.stringify(data)
+      console.log(data)
+      // $('div #map').append(data);
+      $('.list-container').append('<img id="responseImage" class="image-viewer" alt="Response Image" src=' + json + '>')
+    }
+
+  });
+
+
+};
+// var generateMap = function (coordinates) {
+//   // console.log(coordinates)
+//   $.ajax({
+//     url: 'https://image.maps.cit.api.here.com/mia/1.6/mapview?c=37.7719,-122.4218&app_id=6E6ECV8X9yllwqowlbdF&app_code=bGWBR9zg_GlSh-8TQis1Hw&poi=37.776235,-122.423196, 37.775772, -122.409319,37.772,-122.43117&w=800&h=1000',
+//     type: 'GET',
+//     data: {
+//        c: '37.7719,-122.4218', // this is the current location (tech crunch location)
+//        app_id: 'fRTOmoR7FfcvAIuCqt1V',
+//        app_code: 'X1FkzDPPRfhlg2cjfTbT2w',
+//        poi: coordinates,
+//        w: 600,
+//        h: 600
+//        // contentType: 'image/jpeg;charset=UTF-8'
+//      },
+//      success: function (data) {
+//       console.log(data);
+//       json = JSON.stringify(data);
+//       alert(json);
+//       // console.log(typeof(json));
+//       // console.log(json);
+//       // console.log(data)
+//       // alert(JSON.stringify(data))
+//       // $('#map').append(data);
+//      }
+//   })
+// }
 
 
 var getNearbyLocations = function () {
  $("#button").on('click', function (event) {
    console.log("triggered");
    event.preventDefault();
+   console.log(this)
 
-  //  var category = this.serialize()  // get the category information
+
+   var category = $(this).serialize()  // get the category information
+   console.log(category)
 
    // might have to parse out the input here because serialize gives key value hash
 
@@ -26,14 +85,29 @@ var getNearbyLocations = function () {
        xhr.setRequestHeader('Accept', 'application/json');
      },
      success: function (data) {
-       objects = data.results.items
-       parsedData = []
+      // need to create another array or string with all coordinates of each object, then pass into generate map function, coordinates must be string separated by commas
+       var coordinates = new String("");
+       var objects = data.results.items
+       var parsedData = []
+       teststring = objects[1].position[1]
+       // coordinates += teststring
+       console.log(teststring)
        for (var i = 0; i < objects.length; i++){
          objectData = [objects[i].title, objects[i].vicinity, objects[i].distance, objects[i].category.title, i]
          parsedData.push(objectData);
          $("#locationList").append("<div class='detail-container'><span class='place-title'>" + objectData[0] + "</span><span class='place-detail'>" + objectData[1] + "</span><br><span class='place-detail'>" + objectData[2] + "</span><br><span class='place-detail'>" + objectData[3] + "</span></div>");
          $("#locationList").append("<div class='pitch-btn'><form class='' action='' method='post'><input type='hidden' name='item[title]' value='" + objectData[0] + "'><input type='hidden' name='item[address]' value='" + objectData[1] + "'><input type='hidden' name='item[distance]' value='" + objectData[2] + "'><input type='hidden' name='item[category]' value='" + objectData[3] + "'><input type='submit' class='submit-btn' value='PITCH-IT'></form></div>");
+         // append each set of coordinates to a string
+         coordinates += objects[i].position[0] + '%2C'
+         coordinates += objects[i].position[1] + '%2C'
+         // console.log(coordinates)
+         // var coordinates += objects[i].position + ',';
+         // delete last comma in the string
+         // method will delete the very last index of the string
        }
+       var coordinates = coordinates.slice(0, -3);
+       // console.log(coordinates)
+       generateMap(coordinates);
        // parse through information and acquire desired attributes
        // title, location, vicinity, etc
        // append to list
